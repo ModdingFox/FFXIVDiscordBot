21b72a42dee70b5c53a1a82e1475a23d16d34ff0
moddingfox <moddingfox@gmail.com>
2022-07-10 22:41:52 -0700

    Updated radar plugin so it works again

--- ClubSpectrumRadar/ClubSpectrumRadar/Plugin.cs
+++ ClubSpectrumRadar/ClubSpectrumRadar/Plugin.cs
@@ -1,8 +1,18 @@
-﻿using Dalamud.Game.Command;
+﻿using Dalamud.Game.ClientState;
+using Dalamud.Game.ClientState.Objects;
+using Dalamud.Game.ClientState.Objects.Enums;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
+using Dalamud.Logging;
 using Dalamud.Plugin;
-using System.IO;
-using System.Reflection;
+
+using Newtonsoft.Json;
+
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Collections;
 
 namespace ClubSpectrumRadar
 {
@@ -12,30 +22,100 @@ namespace ClubSpectrumRadar
 
         private const string commandName = "/csRadar";
 
+        private Task getPlayersTask;
+        private CancellationTokenSource getPlayersCancellationTokenSource;
+        private ArrayList playerLastSeenList = new ArrayList();
+
+
         private DalamudPluginInterface PluginInterface { get; init; }
+        private ClientState ClientState { get; init; }
         private CommandManager CommandManager { get; init; }
         private Configuration Configuration { get; init; }
+        private ObjectTable ObjectTable { get; init; }
         private PluginUI PluginUi { get; init; }
+
+        private void getPlayersTaskAction()
+        {
+            while (!getPlayersCancellationTokenSource.IsCancellationRequested)
+            {
+                if (this.Configuration.RadarScanEnabled)
+                {
+                    var localPlayer = this.ClientState.LocalPlayer;
+                    if (localPlayer == null)
+                    {
+                        continue;
+                    }
+
+                    ArrayList foundUsers = new ArrayList();
+                    playerPayloadObject playerPayload = new playerPayloadObject();
+
+                    for (var i = 0; i < this.ObjectTable.Length; i++)
+                    {
+                        var actor = this.ObjectTable[i];
+                        if (actor != null && actor.ObjectKind == ObjectKind.Player)
+                        {
+                            PluginLog.Information("Seen: " + actor.Name);
+                            foundUsers.Add(actor.Name.ToString());
+                        }
+                    }
+
+                    foreach (string currentUser in foundUsers)
+                    {
+                        if (!this.playerLastSeenList.Contains(currentUser)) { playerPayload.addPlayers.Add(currentUser); }
+                    }
+
+                    foreach (string currentUser in this.playerLastSeenList)
+                    {
+                        if (!foundUsers.Contains(currentUser)) { playerPayload.removePlayers.Add(currentUser); }
+                    }
+
+                    if (playerPayload.addPlayers.Count > 0 || playerPayload.removePlayers.Count > 0)
+                    {
+                        string payloadString = JsonConvert.SerializeObject(playerPayload);
+                        PluginLog.Information(payloadString);
+
+                        HttpClient http = new HttpClient();
+                        HttpResponseMessage response = http.PostAsync(this.Configuration.UploadTo, new StringContent(payloadString)).Result;
+
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            playerLastSeenList = foundUsers;
+
+                            PluginLog.Information(response.Content.ReadAsStringAsync().Result);
+                            PluginLog.Information("Payload sent");
+                        }
+                        else { PluginLog.Error("Error: Could not contact the server to update list"); }
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                }
+                getPlayersCancellationTokenSource.Token.WaitHandle.WaitOne(1000);
+            }
+        }
 
         public Plugin(
             [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
-            [RequiredVersion("1.0")] CommandManager commandManager)
+            [RequiredVersion("1.0")] CommandManager commandManager,
+            [RequiredVersion("1.0")] ClientState clientState,
+            [RequiredVersion("1.0")] ObjectTable objectTable)
         {
             this.PluginInterface = pluginInterface;
+            this.ClientState = clientState;
             this.CommandManager = commandManager;
-
+           
             this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
             this.Configuration.Initialize(this.PluginInterface);
 
-            // you might normally want to embed resources and load them from the manifest stream
-            var imagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
-            var goatImage = this.PluginInterface.UiBuilder.LoadImage(imagePath);
+            this.ObjectTable = objectTable;
+
             this.PluginUi = new PluginUI(this.Configuration);
 
-            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
-            {
-                HelpMessage = "A useful message to display in /xlhelp"
-            });
+            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand) { HelpMessage = "Config options for the Club Spectrum Radar" });
+
+            getPlayersCancellationTokenSource = new CancellationTokenSource();
+            getPlayersTask = Task.Run(getPlayersTaskAction);
 
             this.PluginInterface.UiBuilder.Draw += DrawUI;
             this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
@@ -47,20 +127,10 @@ namespace ClubSpectrumRadar
             this.CommandManager.RemoveHandler(commandName);
         }
 
-        private void OnCommand(string command, string args)
-        {
-            // in response to the slash command, just display our main ui
-            this.PluginUi.Visible = true;
-        }
+        private void OnCommand(string command, string args) { this.PluginUi.Visible = true; }
 
-        private void DrawUI()
-        {
-            this.PluginUi.Draw();
-        }
+        private void DrawUI() { this.PluginUi.Draw(); }
 
-        private void DrawConfigUI()
-        {
-            this.PluginUi.SettingsVisible = true;
-        }
+        private void DrawConfigUI() { this.PluginUi.SettingsVisible = true; }
     }
 }
---
+++ ClubSpectrumRadar/ClubSpectrumRadar/playerPayloadObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace ClubSpectrumRadar
+{
+    internal class playerPayloadObject
+    {
+        public long timeStamp;
+        public ArrayList addPlayers;
+        public ArrayList removePlayers;
+
+        public playerPayloadObject()
+        {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            timeStamp = (long)t.TotalSeconds;
+            addPlayers = new ArrayList();
+            removePlayers = new ArrayList();
+        }
+    }
+}
