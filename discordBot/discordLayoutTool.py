e8ca9b0120e06dca73d4eb6fc82027b5e96f2b2f
moddingfox <moddingfox@gmail.com>
2022-08-22 11:14:04 +0000

    Updated diff to to save and load layouts/perms

--- discordBot/discordLayoutTool.py
+++ discordBot/discordLayoutTool.py
@@ -1,10 +1,13 @@
+import discordUtils
+
 import discord
 from discord.ext import commands
 from io import BytesIO
 
 import json;
+from json import JSONDecodeError 
 
-class discordDiffToolClass(commands.Cog, name='Discord Diff Tool'):
+class discordLayoutToolClass(commands.Cog, name='Discord Layout Tool'):
     def __init__(self, discordClient):
         self.discordClient = discordClient;
 
@@ -21,6 +24,7 @@ class discordDiffToolClass(commands.Cog, name='Discord Diff Tool'):
             colourAttributes['b'] = colour.b;
             colourAttributes['g'] = colour.g;
             colourAttributes['r'] = colour.r;
+            colourAttributes['value'] = colour.value;
         return colourAttributes;
 
     def permissionsToHashMap(self, permissions):
@@ -250,8 +254,179 @@ class discordDiffToolClass(commands.Cog, name='Discord Diff Tool'):
             categoriesData[category.name] = self.categoryToHashMap(category);
         return categoriesData;
 
-    @commands.command(brief="Dump Server Layout", description="Dump Server Layout to json")
-    async def dumpServerLayout(self, ctx):
+    async def hashMapToRoles(self, ctx, roleData):
+        topRole = ctx.guild.get_member(ctx.me.id).top_role;
+        currentRoles = discordUtils.getRolesByName(ctx.guild);
+        currentRoleKeys = currentRoles.keys();
+        desiredRoleKeys = roleData.keys();
+        createRoles = list(set(desiredRoleKeys).difference(currentRoleKeys));
+        deleteRoles = list(set(currentRoleKeys).difference(desiredRoleKeys));
+        for deleteRole in deleteRoles:
+            if deleteRole != topRole.name:
+                for currentRoleId in currentRoles[deleteRole]:
+                    currentRole = ctx.guild.get_role(currentRoleId);
+                    await currentRole.delete();
+        for createRole in createRoles:
+            if createRole != topRole.name:
+                await ctx.guild.create_role(
+                    name = roleData[createRole]['name'],
+                    permissions = discord.Permissions(permissions = roleData[createRole]['permissions']['value']),
+                    colour  = discord.Colour(roleData[createRole]['colour']['value']),
+                    hoist = roleData[createRole]['hoist'],
+                    mentionable = roleData[createRole]['mentionable'],
+                    reason = "Created by loadServerLayout triggered by {0} - {1}".format(ctx.message.author.name, ctx.message.author.id)
+                );
+        for currentRoleName in roleData.keys():
+            if currentRoleName != topRole.name:
+                currentRoleIds = discordUtils.getRoleByName(ctx.guild, currentRoleName);
+                if currentRoleIds is None:
+                    await ctx.send("Could not find role {0}".format(currentRoleName));
+                elif len(currentRoleIds) != 1:
+                    await ctx.send("Found duplicate for role {0}".format(currentRoleName));
+                else:
+                    currentRole = ctx.guild.get_role(currentRoleIds[0]);
+                    if roleData[currentRoleName]['position'] == 0:
+                        await currentRole.edit(
+                            name = roleData[currentRoleName]['name'],
+                            permissions = discord.Permissions(permissions = roleData[currentRoleName]['permissions']['value']),
+                            colour  = discord.Colour(roleData[currentRoleName]['colour']['value']),
+                            hoist = roleData[currentRoleName]['hoist'],
+                            mentionable = roleData[currentRoleName]['mentionable'],
+                            reason = "Updated by loadServerLayout triggered by {0} - {1}".format(ctx.message.author.name, ctx.message.author.id)
+                        );
+                    else:
+                        await currentRole.edit(
+                            name = roleData[currentRoleName]['name'],
+                            permissions = discord.Permissions(permissions = roleData[currentRoleName]['permissions']['value']),
+                            colour  = discord.Colour(roleData[currentRoleName]['colour']['value']),
+                            hoist = roleData[currentRoleName]['hoist'],
+                            mentionable = roleData[currentRoleName]['mentionable'],
+                            position = roleData[currentRoleName]['position'],
+                            reason = "Updated by loadServerLayout triggered by {0} - {1}".format(ctx.message.author.name, ctx.message.author.id)
+                        );
+
+    async def deleteChannels(self, channels):
+        for channel in channels:
+            await channel.delete();
+
+    async def hashMapToOverwrites(self, ctx, overwritesData):
+        overwrites = {};
+        for currentOverwriteKey in overwritesData.keys():
+            currentRoleIds = discordUtils.getRoleByName(ctx.guild, currentOverwriteKey);
+            if currentRoleIds is None:
+                await ctx.send("Could not find role {0}".format(currentOverwriteKey));
+            elif len(currentRoleIds) != 1:
+                await ctx.send("Found duplicate for role {0}".format(currentOverwriteKey));
+            else:
+                currentRole = ctx.guild.get_role(currentRoleIds[0]);
+                allow = discord.Permissions(permissions = overwritesData[currentOverwriteKey]['allow']['value']);
+                deny = discord.Permissions(permissions = overwritesData[currentOverwriteKey]['deny']['value']);
+                overwrites[currentRole] = discord.PermissionOverwrite.from_pair(allow, deny);
+        return overwrites;
+
+    async def hashMapsToChannels(self, ctx, category, stageChannels, textChannels, voiceChannels):
+        currentChannels = category.channels;
+        currentChannelsKeys = list(map(lambda element: element.name, currentChannels));
+        desiredChannelsKeys = list(stageChannels.keys()) + list(textChannels.keys()) + list(voiceChannels.keys());
+        createChannels = list(set(desiredChannelsKeys).difference(currentChannelsKeys));
+        deleteChannels = list(set(currentChannelsKeys).difference(desiredChannelsKeys));
+        for deleteChannel in deleteChannels:
+            for currentChannel in currentChannels:
+                if currentChannel.name == deleteChannel:
+                    await currentChannel.delete();
+        for createChannel in createChannels:
+            if createChannel in stageChannels.keys():
+                await category.create_stage_channel(
+                    name = stageChannels[createChannel]['name'],
+                    topic = stageChannels[createChannel]['topic'],
+                    overwrites = await self.hashMapToOverwrites(ctx, stageChannels[createChannel]['overwrites']),
+                    reason = "Created by loadServerLayout triggered by {0} - {1}"
+                );
+            elif createChannel in textChannels.keys():
+                await category.create_text_channel(
+                    name = textChannels[createChannel]['name'],
+                    overwrites = await self.hashMapToOverwrites(ctx, textChannels[createChannel]['overwrites']),
+                    topic = textChannels[createChannel]['topic'],
+                    slowmode_delay = textChannels[createChannel]['slowmode_delay'],
+                    nsfw = textChannels[createChannel]['nsfw'],
+                    default_auto_archive_duration = textChannels[createChannel]['default_auto_archive_duration'],
+                    reason = "Created by loadServerLayout triggered by {0} - {1}"
+                );
+            elif createChannel in voiceChannels.keys():
+                await category.create_voice_channel(
+                    name = voiceChannels[createChannel]['name'],
+                    overwrites = await self.hashMapToOverwrites(ctx, voiceChannels[createChannel]['overwrites']),
+                    bitrate = voiceChannels[createChannel]['bitrate'],
+                    user_limit = voiceChannels[createChannel]['user_limit'],
+                    rtc_region = voiceChannels[createChannel]['rtc_region'],
+                    reason = "Created by loadServerLayout triggered by {0} - {1}"
+                );
+            else:
+                await ctx.send("Channel {0} is not of type stageChannel, textChannel, or voiceChannel".format(createChannel));
+        for currentStageChannel in category.stage_channels:
+            await currentStageChannel.edit(
+                 name = stageChannels[currentStageChannel.name]['name'],
+                 position = stageChannels[currentStageChannel.name]['position'],
+                 overwrites = await self.hashMapToOverwrites(ctx, stageChannels[currentStageChannel.name]['overwrites']),
+                 reason = "Updated by loadServerLayout triggered by {0} - {1}"
+            );
+        for currentTextChannel in category.text_channels:
+            await currentTextChannel.edit(
+                 name = textChannels[currentTextChannel.name]['name'],
+                 topic = textChannels[currentTextChannel.name]['topic'],
+                 position = textChannels[currentTextChannel.name]['position'],
+                 nsfw = textChannels[currentTextChannel.name]['nsfw'],
+                 slowmode_delay = textChannels[currentTextChannel.name]['slowmode_delay'],
+                 overwrites = await self.hashMapToOverwrites(ctx, textChannels[currentTextChannel.name]['overwrites']),
+                 default_auto_archive_duration = textChannels[currentTextChannel.name]['default_auto_archive_duration'],
+                 reason = "Updated by loadServerLayout triggered by {0} - {1}"
+            );
+        for currentVoiceChannel in category.voice_channels:
+            await currentVoiceChannel.edit(
+                 name = voiceChannels[currentVoiceChannel.name]['name'],
+                 bitrate = voiceChannels[currentVoiceChannel.name]['bitrate'],
+                 nsfw = voiceChannels[currentVoiceChannel.name]['nsfw'],
+                 user_limit = voiceChannels[currentVoiceChannel.name]['user_limit'],
+                 position = voiceChannels[currentVoiceChannel.name]['position'],
+                 overwrites = await self.hashMapToOverwrites(ctx, voiceChannels[currentVoiceChannel.name]['overwrites']),
+                 rtc_region = voiceChannels[currentVoiceChannel.name]['rtc_region'],
+                 reason = "Updated by loadServerLayout triggered by {0} - {1}"
+            );
+
+    async def hashMapToCategories(self, ctx, categoriesData):
+        currentCategories = ctx.guild.categories;
+        currentCategoriesKeys = list(map(lambda element: element.name, currentCategories));
+        desiredCategoriesKeys = categoriesData.keys();
+        createCategories = list(set(desiredCategoriesKeys).difference(currentCategoriesKeys));
+        deleteCategories = list(set(currentCategoriesKeys).difference(desiredCategoriesKeys));
+        for deleteCategory in deleteCategories:
+            for currentCategory in currentCategories:
+                if currentCategory.name == deleteCategory:
+                    await self.deleteChannels(currentCategory.channels);
+                    await currentCategory.delete();
+        for createCategory in createCategories:
+            await ctx.guild.create_category(
+                name = categoriesData[createCategory]['name'],
+                reason = "Created by loadServerLayout triggered by {0} - {1}"
+            );
+        currentCategories = ctx.guild.categories;
+        for currentCategory in currentCategories:
+            await currentCategory.edit(
+                name = categoriesData[currentCategory.name]['name'],
+                overwrites = await self.hashMapToOverwrites(ctx, categoriesData[currentCategory.name]['overwrites']),
+                position = categoriesData[currentCategory.name]['position'],
+                reason = "Updated by loadServerLayout triggered by {0} - {1}"
+            );
+            await self.hashMapsToChannels(
+                ctx,
+                currentCategory,
+                categoriesData[currentCategory.name]['stage_channels'],
+                categoriesData[currentCategory.name]['text_channels'],
+                categoriesData[currentCategory.name]['voice_channels']
+            );
+
+    @commands.command(brief="Save Server Layout", description="Save server layout to json")
+    async def saveServerLayoutToJSON(self, ctx):
         serverData = {};
         categoriesData = self.categoriesToHashMap(ctx.guild.categories);
         roleData = self.rolesToHashMap(ctx.guild.roles);
@@ -259,4 +434,25 @@ class discordDiffToolClass(commands.Cog, name='Discord Diff Tool'):
         serverData['roleData'] = roleData;
         serverLayoutJSON = json.dumps(serverData, indent=4);
         serverLayoutBytes = bytes(serverLayoutJSON, "utf-8");
-        await ctx.send("Server Layout JSON", file=discord.File(BytesIO(serverLayoutBytes), "serverLayout.json"))
+        await ctx.send("Server Layout JSON", file=discord.File(BytesIO(serverLayoutBytes), "serverLayout.json"));
+
+    @commands.command(brief="Load Server Layout", description="Load server layout from json")
+    async def loadServerLayoutFromJSON(self, ctx):
+        if len(ctx.message.attachments) == 1:
+            serverDataAttachment = await ctx.message.attachments[0].read();
+            try:
+                serverData = json.loads(serverDataAttachment);
+                if "roleData" in serverData.keys():
+                     await self.hashMapToRoles(ctx, serverData["roleData"]);
+                else:
+                    await ctx.send("No roleData. Skipping.");
+                if "categoriesData" in serverData.keys():
+                    await self.hashMapToCategories(ctx, serverData["categoriesData"]);
+                else:
+                    await ctx.send("No categoriesData. Skipping.");
+                await ctx.send("Server layout update complete");
+            except JSONDecodeError as e:
+                await ctx.send("Data recieved is not json");
+        else:
+            await ctx.send("Must attach a single configuration payload file");
+
